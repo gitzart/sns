33bdbffcc4ce7c8e8cf2ea35db05c67fb7fab05a
gitzart <cracker1510@gmail.com>
2018-04-05 23:58:15 +0630

    Fix so DB can work with multiple app instances

--- services/sns/project/sa_wrapper.py
+++ services/sns/project/sa_wrapper.py
@@ -1,5 +1,6 @@
 # SQLAlchemy extension
 
+from flask import current_app
 from sqlalchemy import create_engine, orm
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,30 +9,60 @@ class SQLAlchemy:
     '''A small SQLAlchemy wrapper class.'''
 
     def __init__(self, app=None):
+        self.app = app
         self.Base = declarative_base()
 
         if app is not None:
             self.init_app(app)
 
+    # def init_app(self, app):
+    #     '''Initialize an application for the use
+    #     with this database setup.
+    #     '''
+    #     self.engine = create_engine(
+    #         app.config['DATABASE_URL'],
+    #         convert_unicode=True,
+    #     )
+    #     session_factory = orm.sessionmaker(
+    #         autocommit=False,
+    #         autoflush=False,
+    #         bind=self.engine,
+    #     )
+    #     self.session = orm.scoped_session(session_factory)
+    #     self.Base.query = self.session.query_property()
+    #
+    #     @app.teardown_appcontext
+    #     def shutdown_session(exception=None):
+    #         self.session.remove()
+
     def init_app(self, app):
         '''Initialize an application for the use
         with this database setup.
         '''
-        self.engine = create_engine(
-            app.config['DATABASE_URL'],
-            convert_unicode=True,
-        )
-        session_factory = orm.sessionmaker(
-            autocommit=False,
-            autoflush=False,
-            bind=self.engine,
-        )
-        self.session = orm.scoped_session(session_factory)
+        self.session = self.create_scoped_session()
         self.Base.query = self.session.query_property()
 
         @app.teardown_appcontext
         def shutdown_session(exception=None):
             self.session.remove()
+
+    def create_scoped_session(self):
+        return orm.scoped_session(self.create_session())
+
+    def create_session(self):
+        return orm.sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=self.engine,
+        )
+
+    @property
+    def engine(self):
+        app = self.app or current_app._get_current_object()
+        return create_engine(
+            app.config['DATABASE_URL'],
+            convert_unicode=True,
+        )
 
     def _execute_for_all_tables(self, operation):
         op = getattr(self.Base.metadata, operation)
