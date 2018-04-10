bd91232bc059e8343a41205680a58fa950e9da00
gitzart <cracker1510@gmail.com>
2018-04-10 19:16:02 +0630

    Add CLI commands

--- services/sns/manage.py
+++ services/sns/manage.py
@@ -1,9 +1,24 @@
+import pytest
+
 from flask.cli import FlaskGroup
 
-from project import create_app
+from project import create_app, db
 
 
 cli = FlaskGroup(create_app=create_app)
+
+
+@cli.command()
+def create_db():
+    db.drop_all()
+    db.create_all()
+    db.session.commit()
+
+
+@cli.command()
+def test():
+    '''Run the tests without code coverage'''
+    pytest.main([])
 
 
 if __name__ == '__main__':
