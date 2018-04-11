e8aef287558b4ef2a753b319af6b8e6378c56362
gitzart <cracker1510@gmail.com>
2018-04-12 00:53:31 +0630

    Add options to `test` command

--- services/sns/manage.py
+++ services/sns/manage.py
@@ -1,3 +1,5 @@
+import click
+import coverage
 import pytest
 
 from flask.cli import FlaskGroup
@@ -5,6 +7,16 @@ from flask.cli import FlaskGroup
 from project import create_app, db
 from project.api.models import User, Post, Photo, Comment
 
+
+cov = coverage.Coverage(
+    branch=True,
+    include='project/*',
+    omit=[
+        'project/tests/*',
+        'project/config.py',
+    ]
+)
+cov.start()
 
 cli = FlaskGroup(create_app=create_app)
 
@@ -17,9 +29,18 @@ def create_db():
 
 
 @cli.command()
-def test():
-    '''Run the tests without code coverage'''
-    pytest.main([])
+@click.option('-c', '--coverage', is_flag=True)
+def test(coverage):
+    rv = pytest.main([])
+    if coverage and rv == 0:
+        cov.stop()
+        cov.save()
+        print('Coverage Summary:')
+        cov.report(show_missing=True, skip_covered=True)
+        cov.html_report()
+        cov.erase()
+        return 0
+    return 1
 
 
 if __name__ == '__main__':
