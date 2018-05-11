c8f87844b663e223e49a47b400595b9288d7607c
gitzart <cracker1510@gmail.com>
2018-05-11 22:36:08 +0630

    Refactor `to_gql_enum()` function
    The function now returns the GraphQL Enum type which can act as a
    standalone field, and also work nicely with `graphene.Field` and
    `graphene.Argument` classes.

--- services/sns/project/utils.py
+++ services/sns/project/utils.py
@@ -5,15 +5,31 @@ import graphene
 from project import db
 
 
+# From this response in Stackoverflow
+# http://stackoverflow.com/a/1176023/1072990
+def to_snake_case(camel_str):
+    """Convert CamelCase or mixedCase into snake_case."""
+    s = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', camel_str)
+    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s).lower()
+
+
 def to_gql_enum(py_enum):
     """Convert Python enumeration into GraphQL enumeration."""
 
     def description(enum_member):
         if enum_member is not None:
-            return enum_member.describe()
+            return enum_member.value.describe()
         return py_enum.__doc__
 
-    return graphene.Enum.from_enum(py_enum, description=description)
+    d = {
+        '__doc__': py_enum.__doc__,
+        'description': description,
+    }
+
+    for key, value in py_enum.__members__.items():
+        d[key] = value
+
+    return type(py_enum.__name__, (graphene.Enum,), d)
 
 
 def to_sa_enum(py_enum):
@@ -23,11 +39,3 @@ def to_sa_enum(py_enum):
         name=to_snake_case(py_enum.__name__),
         values_callable=lambda x: [e.value for e in x],
     )
-
-
-# From this response in Stackoverflow
-# http://stackoverflow.com/a/1176023/1072990
-def to_snake_case(camel_str):
-    """Convert CamelCase or mixedCase into snake_case."""
-    s = re.sub(r'(.)([A-Z][a-z]+)', r'\1_\2', camel_str)
-    return re.sub(r'([a-z0-9])([A-Z])', r'\1_\2', s).lower()
