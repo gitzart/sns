1a9bb1ac07d2c2028a63705202235c9f4ba27063
gitzart <cracker1510@gmail.com>
2018-06-02 19:10:53 +0630

    Add graphiql route

--- services/sns/project/api/app.py
+++ services/sns/project/api/app.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
+from flask_graphql import GraphQLView
+
+from .schemas.schema import schema
 
 
 sns_blueprint = Blueprint('sns', __name__)
 
-
-@sns_blueprint.route('/')
-def hello():
-    return 'Hello'
+sns_blueprint.add_url_rule(
+    '/',
+    view_func=GraphQLView.as_view(
+        'graphql', schema=schema, graphiql=True
+    )
+)
