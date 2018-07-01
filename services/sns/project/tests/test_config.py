e9180f747ec6d0ecd848140fb539ac9807072f11
gitzart <cracker1510@gmail.com>
2018-07-01 22:17:18 +0630

    Modify app config

--- services/sns/project/tests/test_config.py
+++ services/sns/project/tests/test_config.py
@@ -7,7 +7,9 @@ def test_development_config(app):
     assert app.debug
     assert app.config['SECRET_KEY'] == 'my_precious'
     assert app.config['SQLALCHEMY_DATABASE_URI'] == os.getenv('DATABASE_URL')
+    assert not app.config['SQLALCHEMY_ECHO']
     assert app.config['DEBUG_TB_ENABLED']
+    assert app.config['BCRYPT_LOG_ROUNDS'] == 4
 
 
 def test_testing_config(app):
@@ -17,11 +19,15 @@ def test_testing_config(app):
     assert app.config['SECRET_KEY'] == 'my_precious'
     assert (app.config['SQLALCHEMY_DATABASE_URI'] ==
             os.getenv('DATABASE_TEST_URL'))
+    assert app.config['SQLALCHEMY_ECHO'] == 'debug'
     assert not app.config['DEBUG_TB_ENABLED']
+    assert app.config['BCRYPT_LOG_ROUNDS'] == 4
 
 
 def test_production_config(app):
     app.config.from_object('project.config.ProductionConfig')
     assert not app.testing
     assert not app.debug
+    assert not app.config['SQLALCHEMY_ECHO']
     assert not app.config['DEBUG_TB_ENABLED']
+    assert app.config['BCRYPT_LOG_ROUNDS'] == 12
